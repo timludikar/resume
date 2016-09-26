5c77de32a96ac10417f484e4a4ae43c467aac283
Tim Ludikar <tim.ludikar@ikandimedia.ca>
2016-09-25 21:41:35 -0400

    Adding paragraph styling and common elements

--- app/components/paragraph/index.js
+++ app/components/paragraph/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import CSSModules from 'react-css-modules';
 
+import styles from './paragraph.styl';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return <p>{this.props.children}</p>;
+    let large = this.props.fontSize ? 'large' : '';
+    let centered = this.props.centered ? 'center' : '';
+    let justified = this.props.justified ? 'justified' : '';
+    let cssClasses = `${large} ${centered} ${justified}`;
+    return <p className={cssClasses}>{this.props.children}</p>;
   }
 }
 
-export default App;
+export default CSSModules(App, styles);
